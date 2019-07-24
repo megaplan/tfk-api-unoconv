65c2f3fd19df063a070ba4587cdfc36b0f024a7b
Artem Golosenko <golosenko.artem@gmail.com>
2019-07-24 20:14:18 +0300

    Генерация напрямую через soffice

--- handlers/index.js
+++ handlers/index.js
@@ -2,84 +2,67 @@
 
 const fs = require('fs')
 const uuid = require('uuid')
-const unoconv = require('unoconv2')
 const formats = require('../lib/data/formats.json')
 const pkg = require('../package.json')
+const { exec } = require('child_process');
 
 module.exports.handleUpload = (request, reply) => {
-  const convertToFormat = request.params.format
-  const data = request.payload
-  if (data.file) {
-    const nameArray = data.file.hapi.filename.split('.')
-    const fileEndingOriginal = nameArray.pop()
-    const temporaryName = uuid.v4()
-    const pathPre = process.cwd() + '/uploads/' + temporaryName
-    const fileNameTempOriginal = pathPre + '.' + fileEndingOriginal
-    const file = fs.createWriteStream(fileNameTempOriginal)
+    const convertToFormat = request.params.format
+    const data = request.payload
+    if (data.file) {
+        const nameArray = data.file.hapi.filename.split('.')
+        const fileEndingOriginal = nameArray.pop()
+        const temporaryName = uuid.v4()
+        const outDir = process.cwd() + '/uploads/'
+        const pathPre = outDir + temporaryName
+        const fileNameTempOriginal = pathPre + '.' + fileEndingOriginal
+        const fileNameTempResult = pathPre + '.' + convertToFormat
+        const file = fs.createWriteStream(fileNameTempOriginal)
 
-    file.on('error', (error) => {
-      console.error(error)
-    })
-
-    data.file.pipe(file)
-
-    data.file.on('end', (err) => {
-      if (err) {
-        console.error(err)
-        reply(err)
-      } else {
-        unoconv.convert(fileNameTempOriginal, convertToFormat, (err, result) => {
-          if (err) {
-            console.error(err)
-            fs.unlink(fileNameTempOriginal, error => {
-              if (error) {
-                console.error(error)
-              } else {
-                console.log(`${fileNameTempOriginal} deleted`)
-              }
-            })
-            reply(err)
-          } else {
-            console.log('finished converting')
-            reply(result)
-              .on('finish', () => {
-                fs.unlink(fileNameTempOriginal, error => {
-                  if (error) {
-                    console.error(error)
-                  } else {
-                    console.log(`${fileNameTempOriginal} deleted`)
-                  }
-                })
-              })
-          }
+        file.on('error', (error) => {
+            console.error(error)
         })
-      }
-    })
-  }
+
+        data.file.pipe(file)
+
+        data.file.on('end', (err) => {
+            if (err) {
+                console.error(err)
+                reply(err)
+            } else {
+                exec('soffice --headless --convert-to '+convertToFormat+' --outdir '+outDir+' '+fileNameTempOriginal, (err, stdout, stderr) => {
+                   if (err) {
+                       reply(err);
+                   }
+                   reply.file(fileNameTempResult);
+                });
+            }
+       })
+    }
 }
 
 module.exports.showFormats = (request, reply) => {
-  reply(formats)
+    reply(formats)
 }
 
 module.exports.showFormat = (request, reply) => {
-  const params = request.params
-  const format = params ? formats[request.params.type] : false
-  if (!format) {
-    reply('Format type not found').code(404)
-  } else {
-    reply(format)
-  }
+    const params = request.params
+    const format = params ? formats[request.params.type] : false
+    if (!format) {
+        reply('Format type not found').code(404)
+    } else {
+        reply(format)
+    }
 }
 
 module.exports.showVersions = (request, reply) => {
-  let versions = {}
-  Object.keys(pkg.dependencies).forEach((item) => {
-    versions[item] = pkg.dependencies[item]
-  })
-  reply(versions)
+    let versions = {}
+    Object.keys(pkg.dependencies).forEach((item) => {
+        versions[item] = pkg.dependencies[item]
+    })
+    reply(versions)
 }
 
 module.exports.healthcheck = (request, reply) => {
-  reply({ uptime: process.uptime() })
+    reply({ uptime: process.uptime() })
 }
